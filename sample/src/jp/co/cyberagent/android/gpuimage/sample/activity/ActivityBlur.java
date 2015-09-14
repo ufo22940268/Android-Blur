c70e8fb887cfcd5ca509bc7e64c4cbfdb1b6d700
Frank Cheng <ufo22940268@gmail.com>
2015-09-14 19:10:54 +0800

    Isolate blur activity for further demo development.

--- sample/src/jp/co/cyberagent/android/gpuimage/sample/activity/ActivityBlur.java
+++ sample/src/jp/co/cyberagent/android/gpuimage/sample/activity/ActivityBlur.java
@@ -20,44 +20,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Toast;
 
 import jp.co.cyberagent.android.gpuimage.GPUImageBoxBlurFilter;
 import jp.co.cyberagent.android.gpuimage.GPUImageFilter;
 import jp.co.cyberagent.android.gpuimage.GPUImageView;
-import jp.co.cyberagent.android.gpuimage.GPUImageView.OnPictureSavedListener;
-import jp.co.cyberagent.android.gpuimage.sample.GPUImageFilterTools;
-import jp.co.cyberagent.android.gpuimage.sample.GPUImageFilterTools.FilterAdjuster;
-import jp.co.cyberagent.android.gpuimage.sample.GPUImageFilterTools.OnGpuImageFilterChosenListener;
 import jp.co.cyberagent.android.gpuimage.sample.R;
 
-public class ActivityBlur extends Activity implements OnSeekBarChangeListener,
-        OnClickListener, OnPictureSavedListener {
+public class ActivityBlur extends Activity {
 
     private static final int REQUEST_PICK_IMAGE = 1;
     private GPUImageFilter mFilter;
-    private FilterAdjuster mFilterAdjuster;
     private GPUImageView mGPUImageView;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gallery);
-        ((SeekBar) findViewById(R.id.seekBar)).setOnSeekBarChangeListener(this);
-        findViewById(R.id.button_choose_filter).setOnClickListener(this);
-        findViewById(R.id.button_save).setOnClickListener(this);
-
+        setContentView(R.layout.activity_blur);
         mGPUImageView = (GPUImageView) findViewById(R.id.gpuimage);
-
-//        Intent { dat=content://com.google.android.apps.photos.contentprovider/-1/1/content://media/external/images/media/93657/ACTUAL/399126742 flg=0x1 (has clip) }
         handleImage(Uri.parse("content://media/external/images/media/64"));
-//        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-//        photoPickerIntent.setType("image/*");
-//        startActivityForResult(photoPickerIntent, REQUEST_PICK_IMAGE);
     }
 
     @Override
@@ -77,67 +57,12 @@ public class ActivityBlur extends Activity implements OnSeekBarChangeListener,
         }
     }
 
-    @Override
-    public void onClick(final View v) {
-        switch (v.getId()) {
-            case R.id.button_choose_filter:
-                GPUImageFilterTools.showDialog(this, new OnGpuImageFilterChosenListener() {
-
-                    @Override
-                    public void onGpuImageFilterChosenListener(final GPUImageFilter filter) {
-                        switchFilterTo(filter);
-                        mGPUImageView.requestRender();
-                    }
-
-                });
-                break;
-            case R.id.button_save:
-                saveImage();
-                break;
-
-            default:
-                break;
-        }
-
-    }
-
-    @Override
-    public void onPictureSaved(final Uri uri) {
-        Toast.makeText(this, "Saved: " + uri.toString(), Toast.LENGTH_SHORT).show();
-    }
-
-    private void saveImage() {
-        String fileName = System.currentTimeMillis() + ".jpg";
-        mGPUImageView.saveToPictures("GPUImage", fileName, this);
-//        mGPUImageView.saveToPictures("GPUImage", fileName, 1600, 1600, this);
-    }
-
     private void switchFilterTo(final GPUImageFilter filter) {
         if (mFilter == null
                 || (filter != null && !mFilter.getClass().equals(filter.getClass()))) {
             mFilter = filter;
             mGPUImageView.setFilter(mFilter);
-            mFilterAdjuster = new FilterAdjuster(mFilter);
-
-            findViewById(R.id.seekBar).setVisibility(
-                    mFilterAdjuster.canAdjust() ? View.VISIBLE : View.GONE);
         }
-    }
-
-    @Override
-    public void onProgressChanged(final SeekBar seekBar, final int progress, final boolean fromUser) {
-        if (mFilterAdjuster != null) {
-            mFilterAdjuster.adjust(progress);
-        }
-        mGPUImageView.requestRender();
-    }
-
-    @Override
-    public void onStartTrackingTouch(final SeekBar seekBar) {
-    }
-
-    @Override
-    public void onStopTrackingTouch(final SeekBar seekBar) {
     }
 
     private void handleImage(final Uri selectedImage) {
